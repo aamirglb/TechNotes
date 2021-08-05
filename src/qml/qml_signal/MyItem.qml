4c28e7e02cc5f860314bffea6986e82603809bf8
Aamir <aamirglb@outlook.com>
2021-08-05 10:55:05 +0400

    QML func call test

--- src/qml/qml_signal/MyItem.qml
+++ src/qml/qml_signal/MyItem.qml
@@ -7,6 +7,14 @@ Item {
     property int clickCount: 0
     signal qmlSignal(string msg)
 
+    function readValues(anArray, anObject) {
+        for(var i=0; i<anArray.length; ++i)
+            console.log("Array Item:", anArray[i])
+
+        for(var prop in anObject) {
+            console.log("Object Item:", prop, "=", anObject[prop])
+        }
+    }
     Rectangle {
         width: 80; height: 80
         anchors.centerIn: parent
--- src/qml/qml_signal/main.cpp
+++ src/qml/qml_signal/main.cpp
@@ -3,6 +3,10 @@
 #include <QQuickView>
 #include <QQuickItem>
 
+#include <QVariantList>
+#include <QVariantMap>
+#include <QDate>
+
 class MyClass: public QObject {
     Q_OBJECT
 public slots:
@@ -22,6 +26,18 @@ int main(int argc, char *argv[]) {
 
     if(!item)
         qDebug() << "Item is NULL";
+
+    QVariantList list;
+    list << 10 << QColor(Qt::green) << "bottles" << 3.1425;
+
+    QVariantMap map;
+    map.insert("language", "QML");
+    map.insert("released", QDate(2010, 9, 21));
+
+    QMetaObject::invokeMethod(item, "readValues",
+        Q_ARG(QVariant, QVariant::fromValue(list)),
+        Q_ARG(QVariant, QVariant::fromValue(map))
+    );
 
     MyClass myClass;
     QObject::connect(item, SIGNAL(qmlSignal(QString)), &myClass, SLOT(cppSlot(QString)));
