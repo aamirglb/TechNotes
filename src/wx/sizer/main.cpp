150f7528e6d37be8b6115a8fc06ba95631f88591
Aamir <aamir.ali@cartenav.com>
2022-12-25 00:12:12 +0400

    wxWidgets example programs.

---
+++ src/wx/sizer/main.cpp
@@ -0,0 +1,47 @@
+#include "wx/wx.h"
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+};
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello wxWidgets", wxDefaultPosition, wxDefaultSize);
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+    : wxFrame(nullptr, wxID_ANY, title, pos, size)
+{
+    wxPanel *panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxPanel *panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
+
+    wxPanel *panel_bottom_right = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+    panel_bottom_right->SetBackgroundColour(wxColor(200, 200, 100));
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(panel_top, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
+
+    wxSizer *sizer_bottom = new wxBoxSizer(wxHORIZONTAL);
+    sizer_bottom->Add(panel_bottom, 1, wxEXPAND | wxRIGHT, 10);
+    sizer_bottom->Add(panel_bottom_right, 0, wxEXPAND, 0);
+
+    sizer->Add(sizer_bottom, 1, wxEXPAND | wxALL, 10);
+
+    this->SetSizerAndFit(sizer);
+}
+
