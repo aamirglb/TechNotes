cb39a9ff3264b9876b9384ebba7360d031c3b670
Aamir <aamirglb@users.noreply.github.com>
2023-03-07 19:56:41 +0400

    dotnet examples

--- src/dotnet/cs11/vscode-workspace/myenv/Program.cs
+++ src/dotnet/cs11/vscode-workspace/myenv/Program.cs
@@ -5,6 +5,43 @@ using static System.Console;
 System.Data.DataSet ds;
 HttpClient client;
 
+Animal?[] animals = new Animal?[]
+{
+    new Cat { Name = "Karen", Born = new(year:2022, month: 8, day:23),
+        Legs = 4, IsDomestic = true },
+    null,
+    new Cat { Name = "Mufasa", Born = new(year:1994, month: 6, day: 12)},
+    new Spider { Name = "spider", Born = DateTime.Today, IsPoisonous = true },
+    new Spider { Name = "Captain Furry", Born = DateTime.Today }
+};
+
+foreach( Animal? animal in animals)
+{
+    string message;
+    switch(animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is named {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is named {cat.Name}.";
+            break;
+        default:
+            message = $"The animal named {animal.Name} is a {animal.GetType().Name}.";
+            break;
+        case Spider spider when spider.IsPoisonous:
+            message = $"The {spider.Name} spider is poisonous.";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+    }
+    WriteLine($"switch statement: {message}");
+}
+return;
 WriteLine(Environment.CurrentDirectory);
 WriteLine(Environment.OSVersion.VersionString);
 WriteLine(Environment.UserName);
@@ -121,3 +158,44 @@ foreach(AssemblyName name in myApp.GetReferencedAssemblies())
 
 // // File.WriteAllBytes("128bytes.txt", bytes);
 // File.WriteAllBytes("256bytes.txt", bytes);
+{
+    object o = "3";
+    int j = 4;
+    if(o is int i)
+    {
+        WriteLine($"{i} x {j} = {i*j}");
+    }
+    else
+    {
+        WriteLine("o is not an int");
+    }
+
+    int number = Random.Shared.Next(1, 10);
+    WriteLine($"My random number is {number}");
+
+    switch(number)
+    {
+        case 1:
+            WriteLine("One");
+            break;
+        case 2:
+            WriteLine("Two");
+            goto case 1;
+        case 3:
+        case 4:
+            WriteLine("Three or four");
+            goto case 1;
+        case 5:
+            goto A_label;
+        default:
+            WriteLine("Default");
+            break;
+    }
+
+    WriteLine("After end of switch");
+    A_label:
+    WriteLine($"After A_label");
+}
+
+
+// STANAG 4509
