ade375ee1c0092d9aa197f17db4b01f36c304ff3
Aamir <aamirglb@outlook.com>
2025-10-14 07:19:38 +0400

    Updated follow target sketch

--- src/js/gswp5/follow_target.js
+++ src/js/gswp5/follow_target.js
@@ -1,21 +1,28 @@
 let startX = 20, stopX = 160;
 let startY = 30, stopY = 80;
 
-// let x = startX, y = startY;
-let x = 0, y = 0;
+let x = 10, y = 10;
 let step = 0.005;
 let pct = 0.0;
 
 let targetPos = false;
 let targets = [];
 let currTarget = null;
+let targetColors = ["red", "maroon", "purple", "fuchsia", "green", "lime", "yellow", "blue","teal", "aqua"];
+let colorIdx = 0;
+let numOfSteps = 0;
 
 function setup() {
-  createCanvas(600, 600);
+  canvas = createCanvas(800, 800);
+  canvas.parent('p5-container');
+  textFont("Open Sans");
 }
 
 function mouseClicked() {
-  targets.push({x: mouseX, y: mouseY});
+  // let colr = targetColors[Math.floor(Math.random() * targetColors.length)];
+  let colr = targetColors[colorIdx % targetColors.length];
+  colorIdx += 1;
+  targets.push({x: mouseX, y: mouseY, color: colr});
 }
 function draw() {
   background(0);
@@ -24,6 +31,7 @@ function draw() {
     currTarget = targets[0];
     startX = x;
     startY = y;
+
     console.log("target: ", currTarget);
   }
   
@@ -32,23 +40,26 @@ function draw() {
     stopY = currTarget.y;
     let d = Math.abs(dist(stopX, stopY, x, y));
     
-    if( d > 5) {
-      // higher the distance, higher the step
-      // step = map(d, 0, 800, 0.005, 0.005*5);
+    if( d > 6 ) {
       if(pct < 1.0) {
+        numOfSteps += 1;
         x = startX + ((stopX - startX) * pct);
         y = startY + ((stopY - startY) * pct);
         pct += step;
       }
       
-      // console.log("x=",x,"y=",y);
       // console.log("d = ", d, " pct = ", pct, "x-delta = ", (stopX - startX) * pct);
-      // console.log(x, y);
     } else {
+       const t = document.getElementById('text-container');
+       const p = document.createElement('p');
+       p.innerHTML = `no of steps to reach target: ${numOfSteps}`;
+       t.appendChild(p);
+
       // remove the current target
       currTarget = null;
       targets.shift();
       pct = 0.0;
+      numOfSteps = 0;
     }
     
   }
@@ -57,27 +68,25 @@ function draw() {
   fill(255);
   ellipse(x, y, 20, 20);
   
-  // let d = Math.abs(dist(stopX, stopY, x, y));
-  // step = map(d, 0, 800, 0.005, 0.005*5);
-  // // console.log(d, " - ", step);
-  // if(pct < 1.0) {
-  // // if(true) {
-  //   x = startX + ((stopX - startX) * pct);
-  //   y = startY + ((stopY - startY) * pct);
-  //   pct += step;
-  // }
-  // noStroke();
-  // fill(255);
-  // ellipse(x, y, 20, 20);
-  
   for(let target of targets) {
     noStroke();
-    fill(255, 0, 0);
+    fill(target.color);
     circle(target.x, target.y, 10);
   }
-  // if(targetPos) {
-  //   noStroke();
-  //   fill(255, 0, 0);
-  //   circle(stopX, stopY, 10);
-  // }
+
+  noStroke();
+  fill("white");
+  text("Targets: ", 5, height - 30);
+
+  let idx = 0;
+  for(let target of targets) {
+    fill(target.color);
+    let x = 50 + (idx * 20) + 20; 
+    circle(x, height - 35, 10);
+    idx += 1;
+  }
+
+  noStroke();
+  fill("white");
+  text(`Pending Targets: ${targets.length}`, 5, height - 10);
 }
